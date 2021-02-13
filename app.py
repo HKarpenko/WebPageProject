b04d1d4b9fab9cd6f2ac0ca50004c537dc304d2d
HKarpenko <45368181+HKarpenko@users.noreply.github.com>
2021-02-13 18:28:18 +0100

    Add Libs, base structure creates

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return "Hello World!"
+
+@app.route('/start')
+def start():
+    return render_template("start.html")
+
+@app.route('/login', methods=["GET","POST"])
+def login():
+    return render_template("start.html")
+
+if __name__ == '__main__':
+    app.run()
