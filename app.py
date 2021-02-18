ff285abe308768ac31485a8ab16ed83af3d93361
HKarpenko <45368181+HKarpenko@users.noreply.github.com>
2021-02-18 01:20:38 +0100

    Start bootstrap positioning on home page

--- app.py
+++ app.py
@@ -4,16 +4,26 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello World!"
+    return render_template("home.html")
+
 
 @app.route('/start')
 def start():
     return render_template("start.html")
 
+
 @app.route('/login', methods=["GET","POST"])
 def login():
-    
-    return render_template("start.html")
+    error = None
+    if request.method == "POST":
+        if request.form["uname"] != "admin":
+            error = "Permission denied."
+        elif request.form["uname"] == "" and request.form["psw"] == "":
+            error = "Empty fields"
+        else:
+            return redirect(url_for("start"))
+    return render_template("login.html", error=error)
+
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug = True)
