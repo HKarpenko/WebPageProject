8087e91b125fe19a599f96c12f407119d0833696
HKarpenko <45368181+HKarpenko@users.noreply.github.com>
2021-03-06 16:30:13 +0100

    Add order form processing and telegram bot connect

--- app.py
+++ app.py
@@ -1,19 +1,59 @@
-from flask import Flask, render_template, redirect, url_for, request
-app = Flask(__name__)
+import os
+from flask import Flask, render_template, redirect, url_for, request, flash
+from werkzeug.utils import secure_filename
 
+import telebot
+
+bot = telebot.AsyncTeleBot(token='1644959765:AAEn325HJiB-JFsMA5D2RqZoT3LBmoC1ZZg')
+my_chat_id = 510305747
+
+UPLOAD_FOLDER = 'static/uploads'
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+
+"""
+@bot.message_handler(commands=['help', 'start'])
+def send_welcome(message):
+    bot.send_message(my_chat_id, "Hello")
+"""
+
+def telegram_send(name="None",email="None",tel="None",order_type="None",order_message="None",files=None):
+    message = "Имя пользователя: "+str(name)+"\n"
+    message += "Email: " + str(email) + "\n"
+    message += "Номер телефона: " + str(tel) + "\n"
+    message += "Тип заказа: " + str(order_type) + "\n"
+    message += "Главное сообщение: " + str(order_message)
+    bot.send_message(my_chat_id, message)
 
 @app.route('/')
 def hello():
     return render_template("home.html")
 
-
-@app.route('/start')
-def start():
-    return render_template("start.html")
-
 @app.route('/order')
 def order():
     return render_template("order.html")
+
+@app.route('/order', methods=["POST"])
+def upload_order():
+    if request.method == "POST":
+        if request.form["Name"] and request.form["Tel"] and request.form["Type"]:
+            name = request.form["Name"]
+            tel = request.form["Tel"]
+            email = request.form["Email"]
+            order_type = request.form["Type"]
+            order_message = request.form["Message"]
+            files = request.files.getlist("Files")
+            for file in files:
+                if file.filename != '':
+                    filename = secure_filename(file.filename)
+                    file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            telegram_send(name=name, tel=tel, email=email, order_type=order_type, order_message=order_message,files=files)
+            flash('Message were successfully sended')
+    return redirect(request.url)
+
 
 @app.route('/tattoos')
 def tattoos():
@@ -23,18 +63,9 @@ def tattoos():
 def sketches():
     return render_template("sketches.html")
 
-@app.route('/login', methods=["GET","POST"])
-def login():
-    error = None
-    if request.method == "POST":
-        if request.form["uname"] != "admin":
-            error = "Permission denied."
-        elif request.form["uname"] == "" and request.form["psw"] == "":
-            error = "Empty fields"
-        else:
-            return redirect(url_for("start"))
-    return render_template("login.html", error=error)
-
+@app.route('/about')
+def about():
+    return render_template("about.html")
 
 if __name__ == '__main__':
     app.run(debug = True)
