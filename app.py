a81685e7f5cc95654e9b24e783fc4c96b0215bd1
HKarpenko <45368181+HKarpenko@users.noreply.github.com>
2021-03-07 14:24:03 +0100

    env restart, adding size limits of sended files

--- app.py
+++ app.py
@@ -2,18 +2,24 @@ import os
 import tempfile
 from flask import Flask, render_template, redirect, request, flash
 from werkzeug.utils import secure_filename
+from celery import Celery
 
 import telebot
 
 bot = telebot.TeleBot(token='1644959765:AAEn325HJiB-JFsMA5D2RqZoT3LBmoC1ZZg')
 my_chat_id = 510305747
 
-UPLOAD_FOLDER = 'static/uploads'
 ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+#app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+#app.config['CELERY_BROKER_URL'] = 'redis://localhost:6379/0'
+#app.config['CELERY_RESULT_BACKEND'] = 'redis://localhost:6379/0'
+
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+#celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])
+#celery.conf.update(app.config)
 
 """
 @bot.message_handler(commands=['help', 'start'])
@@ -21,6 +27,7 @@ def send_welcome(message):
     bot.send_message(my_chat_id, "Hello")
 """
 
+#@celery.task
 def telegram_send(name="None",email="None",tel="None",order_type="None",order_message="None",files=None):
     message = "Имя пользователя: "+str(name)+"\n"
     message += "Email: " + str(email) + "\n"
